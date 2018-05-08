461c2d131e08edca6b6ff4d92e3f0358bd7fee34
Joseph Parker <jparker010190@gmail.com>
2018-05-08 02:38:11 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,6 +1,6 @@
 module Dance
   def twirl
-    "I'm twirling"
+    "I'm twirling!"
   end
   def jump
     "Look how high I'm jumping!"
@@ -11,4 +11,5 @@ module Dance
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
+
 end
